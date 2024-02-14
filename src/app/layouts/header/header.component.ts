94f9f097791114a842bc93a2e167afea701c86c2
Victoriia-Ripka <vikaripka@gmail.com>
2024-02-14 11:59:15 +0100

    footer and header for laptop

--- src/app/layouts/header/header.component.ts
+++ src/app/layouts/header/header.component.ts
@@ -1,12 +1,42 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+const languages = [
+  { value: 'german', currency: "EURO", language: 'DE' },
+  { value: 'polish', currency: "PLN", language: 'PL' },
+  { value: 'japanese', currency: "JPY", language: 'JAP' },
+  { value: 'english', currency: "USD", language: 'ENG' },
+];
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.scss'
 })
 export class HeaderComponent {
+  languages: Array<any> = languages
+  isMenuOpened: boolean = false;
+  isModalOpen: boolean = false;
+  selectedLanguage: string = 'ENG';
+  selectedCurrency: string = 'USD';
 
+  toggleMenu() {
+    this.isMenuOpened = !this.isMenuOpened
+  }
+
+  toggleModal() {
+    this.isModalOpen = !this.isModalOpen
+  }
+
+  selectLanguage(language: any): void {
+    this.selectedLanguage = language;
+  }
+
+  selectCurrency(currency: any): void {
+    this.selectedCurrency = currency;
+  }
+
+  
 }
