7fc5881d1b13cd3a1569a6784a6dcf17f27a25c4
Victoriia-Ripka <vikaripka@gmail.com>
2024-02-22 22:17:29 +0100

    product hero and doses

--- src/app/pages/product/components/product-dose/product-dose.component.ts
+++ src/app/pages/product/components/product-dose/product-dose.component.ts
@@ -1,13 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'product-dose',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-dose.component.html',
   styleUrl: './product-dose.component.scss'
 })
 export class ProductDoseComponent {
   @Input()
   product: any
+
+  addPackageToCart(id: number): void {
+    console.log(id)
+  }
 }
--- src/app/pages/product/components/product-info/product-info.component.ts
+++ src/app/pages/product/components/product-info/product-info.component.ts
@@ -1,13 +1,21 @@
+import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'product-info',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './product-info.component.html',
   styleUrl: './product-info.component.scss'
 })
 export class ProductInfoComponent {
   @Input()
   product: any
+
+  openedSection: "desc" | "reviews" = "reviews"
+
+  changeOpenedSection(section: "desc" | "reviews") {
+    this.openedSection = section
+  }
 }
+
