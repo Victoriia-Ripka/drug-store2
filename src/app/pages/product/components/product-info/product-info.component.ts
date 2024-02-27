3c8f09c97d587abd2405e401cfdc33b712c335d0
Victoriia-Ripka <vikaripka@gmail.com>
2024-02-27 14:31:57 +0100

    product mobile done

--- src/app/pages/product/components/product-info/product-info.component.ts
+++ src/app/pages/product/components/product-info/product-info.component.ts
@@ -1,12 +1,16 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'product-info',
-  standalone: true,
-  imports: [CommonModule],
   templateUrl: './product-info.component.html',
-  styleUrl: './product-info.component.scss'
+  styleUrl: './product-info.component.scss',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  standalone: true
 })
 export class ProductInfoComponent {
   @Input()
@@ -16,6 +20,24 @@ export class ProductInfoComponent {
 
   changeOpenedSection(section: "desc" | "reviews") {
     this.openedSection = section
+  }
+
+  reviewForm = new FormGroup({
+    title: new FormControl('', Validators.required),
+    content: new FormControl('', Validators.required),
+  });
+
+  get titleControl() {
+    return this.reviewForm.get('title') as FormControl;
+  }
+
+  get contentControl() {
+    return this.reviewForm.get('content') as FormControl;
+  }
+
+  sendData() {
+    console.log(this.reviewForm.value)
+    this.reviewForm.reset()
   }
 }
 
