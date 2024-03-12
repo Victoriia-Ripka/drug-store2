756c223436876b7f9f6e87077e5763735d417747
Victoriia-Ripka <vikaripka@gmail.com>
2024-03-12 08:40:33 +0100

    user account orders done

--- src/app/pages/account/components/orders/orders.component.ts
+++ src/app/pages/account/components/orders/orders.component.ts
@@ -1,12 +1,40 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormGroup, Validators, FormControl, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'account-orders',
-  standalone: true,
-  imports: [],
   templateUrl: './orders.component.html',
-  styleUrl: './orders.component.scss'
+  styleUrl: './orders.component.scss',
+  imports: [
+    ReactiveFormsModule,
+    CommonModule
+  ],
+  standalone: true
 })
 export class OrdersComponent {
+  orders: any = []
 
+  orderForm = new FormGroup({
+    number: new FormControl('', Validators.required),
+    time: new FormControl('', Validators.required),
+    orders: new FormControl('', Validators.required),
+  });
+
+  get numberControl() {
+    return this.orderForm.get('number') as FormControl;
+  }
+
+  get timeControl() {
+    return this.orderForm.get('time') as FormControl;
+  }
+
+  get ordersControl() {
+    return this.orderForm.get('orders') as FormControl;
+  }
+
+  sendData() {
+    console.log(this.orderForm.value)
+    this.orderForm.reset()
+  }
 }
