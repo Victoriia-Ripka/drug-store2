22cb45899e633f51ab424f8faf6d97c84cac94b1
Victoriia-Ripka <vikaripka@gmail.com>
2024-03-27 14:47:30 +0100

    testimonials swiper done

--- src/app/pages/home/components/testimonials/testimonials.component.ts
+++ src/app/pages/home/components/testimonials/testimonials.component.ts
@@ -1,4 +1,34 @@
 import { Component } from '@angular/core';
+import Swiper from 'swiper';
+import { SwiperOptions } from 'swiper/types';
+
+const swiperParams: SwiperOptions = {
+  slidesPerView: 3,
+  spaceBetween: 60,
+  direction: 'vertical',
+  enabled: false,
+  loop: false,
+  allowTouchMove: false,
+  pagination: {
+    el: '.custom-pagination',
+    renderCustom: (_, current, total) => {
+      return current + ' of ' + total;
+    }
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  },
+  breakpoints: {
+    1600: {
+      spaceBetween: 42,
+      enabled: true,
+      centeredSlides: true,
+      loop: true,
+      direction: 'horizontal',
+    }
+  }
+};
 
 @Component({
   selector: 'testimonials',
@@ -6,4 +36,42 @@ import { Component } from '@angular/core';
   templateUrl: './testimonials.component.html',
   styleUrls: ['./testimonials.component.scss']
 })
-export class TestimonialsComponent { }
+export class TestimonialsComponent {
+  mySwiper: Swiper | undefined;
+  slidesQuantity: number = 3
+  currentPage: number = 1;
+  slidesAmount = 5
+
+  ngAfterViewInit() {
+    this.mySwiper = new Swiper('#reviewsSwiper', swiperParams);
+  }
+
+  ngOnChanges() {
+    if (this.slidesQuantity) {
+      this.slidesQuantity = Math.max(1, this.slidesQuantity)
+    }
+  }
+
+  nextSlide() {
+    if (this.mySwiper) {
+      this.mySwiper.slideNext();
+      if (this.currentPage == this.slidesAmount) {
+        this.currentPage = 1
+      } else {
+        this.currentPage += 1
+      }
+    }
+  }
+
+  prevSlide() {
+    if (this.mySwiper) {
+      this.mySwiper.slidePrev();
+      if (this.currentPage == 1) {
+        this.currentPage = this.slidesAmount
+      } else {
+        this.currentPage -= 1
+      }
+    }
+  }
+
+ }
